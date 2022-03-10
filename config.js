df7848c2cd16bf2799e75da104cde2bf37a9d443
hathechi <97241150+hathechi@users.noreply.github.com>
2022-03-10 17:49:39 +0700

    Add files via upload
    new code

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/le.suri.9081';
+const eventName = 'Chúc Mừng Sinh Nhật ';
+const titleCard = 'Tặng Người Ấy';
+const contentCard = 'Chúc honey của anh luôn luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau nữa !';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hinh-em.jpg';
+const base64 = 'hinh-em2.jpeg';
+const giftImageBase64 = "data:hinh-em2.jpeg;base64, " + base64;
